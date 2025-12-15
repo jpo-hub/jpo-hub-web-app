91764f7ec2eb3c8d701be2fd647ca66c807a2ee8
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-12-15 09:42:34 +0100

    feat (module) : add atelier module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { CandidatsModule } from './module/candidats/candidats.module';
+import { AteliersModule } from './module/ateliers/ateliers.module';
 
 @Module({
-  imports: [PrismaModule, CandidatsModule],
+  imports: [PrismaModule, CandidatsModule, AteliersModule],
   controllers: [],
   providers: [],
 })
