37d7d52156398bae424270f84414acbf5b5fd168
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-12-17 11:17:25 +0100

    feat (module): add service scoring

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CandidatsModule } from './module/candidats/candidats.module';
 import { AteliersModule } from './module/ateliers/ateliers.module';
 import { ConfigModule } from '@nestjs/config';
+import { ScoringModule } from './module/scoring/scoring.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     CandidatsModule,
     AteliersModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ScoringModule,
   ],
   controllers: [],
   providers: [],
