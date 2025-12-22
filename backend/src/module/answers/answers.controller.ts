2c9fea7e66de7eca28e070e1d40edefc207d3a1a
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-12-22 15:10:34 +0100

    feat (answer.service) : Refactor answers module and update DTO and error gestion

--- backend/src/module/answers/answers.controller.ts
+++ backend/src/module/answers/answers.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AnswersService } from './answers.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { AnswersService, ResponseDto } from './answers.service';
 import { CreateAnswerDto } from './dto/create-answer.dto';
 import { UpdateAnswerDto } from './dto/update-answer.dto';
 
@@ -8,27 +16,30 @@ export class AnswersController {
   constructor(private readonly answersService: AnswersService) {}
 
   @Post()
-  create(@Body() createAnswerDto: CreateAnswerDto) {
+  create(@Body() createAnswerDto: CreateAnswerDto): Promise<ResponseDto> {
     return this.answersService.create(createAnswerDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<ResponseDto[]> {
     return this.answersService.findAll();
   }
 
   @Get(':uid')
-  findOne(@Param('uid') uid: string) {
+  findOne(@Param('uid') uid: string): Promise<ResponseDto> {
     return this.answersService.findOne(uid);
   }
 
   @Patch(':uid')
-  update(@Param('uid') uid: string, @Body() updateAnswerDto: UpdateAnswerDto) {
+  update(
+    @Param('uid') uid: string,
+    @Body() updateAnswerDto: UpdateAnswerDto,
+  ): Promise<ResponseDto> {
     return this.answersService.update(uid, updateAnswerDto);
   }
 
   @Delete(':uid')
-  remove(@Param('uid') uid: string) {
+  remove(@Param('uid') uid: string): Promise<ResponseDto> {
     return this.answersService.remove(uid);
   }
 }
--- backend/src/module/answers/dto/create-answer.dto.ts
+++ backend/src/module/answers/dto/create-answer.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAnswerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsObject, IsString } from 'class-validator';
+
+export class CreateAnswerDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  readonly label: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  questionUid: string;
+
+  @ApiProperty({ example: { informatique: 2, cybersecurite: 1 } })
+  @IsObject()
+  filieres?: Record<string, number>;
+}
