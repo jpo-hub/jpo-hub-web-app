fe81caf75223a8b8f8ec381d2c84f7df5ad9404a
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-12-22 14:41:48 +0100

    feat (modules): add answer service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ScoringModule } from './module/scoring/scoring.module';
 import { FilieresModule } from './module/filieres/filieres.module';
 import { QuestionsModule } from './module/questions/questions.module';
+import { AnswersModule } from './module/answers/answers.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { QuestionsModule } from './module/questions/questions.module';
     FilieresModule,
     ConfigModule.forRoot({ isGlobal: true }),
     QuestionsModule,
+    AnswersModule,
   ],
   controllers: [],
   providers: [],
