2afe77bba01f45e795e891b18cde5decf9ccc44e
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-12-22 14:42:29 +0100

    feat (answer.module) : Add PrismaModule import to AnswersModule

--- backend/src/module/answers/answers.module.ts
+++ backend/src/module/answers/answers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnswersService } from './answers.service';
 import { AnswersController } from './answers.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AnswersController],
   providers: [AnswersService],
 })
