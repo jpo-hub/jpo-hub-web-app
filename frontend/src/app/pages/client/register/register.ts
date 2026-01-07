a724a9767d755e18013c712ad395c966d3f05d84
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-07 16:04:35 +0100

    ci (register) : Implement client registration form UI and logic

--- frontend/src/app/pages/client/register/register.ts
+++ frontend/src/app/pages/client/register/register.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { InputForm } from '../../../shared/components/input-form/input-form';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {ButtonPrimary} from '../../../shared/components/button-primary/button-primary';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    InputForm,
+    ReactiveFormsModule,
+    ButtonPrimary
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  form = new FormGroup({
+    nom: new FormControl('', [Validators.required]),
+    prenom: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    dateNaissance: new FormControl('', [Validators.required])
+  });
 
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Formulaire valide:', this.form.value);
+    } else {
+      console.log('Formulaire invalide');
+      this.form.markAllAsTouched();
+    }
+  }
 }
