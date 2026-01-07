f068516519db1435a91aad92006513fb77eef313
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-07 16:53:09 +0100

    feat (register) : add consentement

--- frontend/src/app/pages/client/register/register.ts
+++ frontend/src/app/pages/client/register/register.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { InputForm } from '../../../shared/components/input-form/input-form';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {Component} from '@angular/core';
+import {InputForm} from '../../../shared/components/input-form/input-form';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ButtonPrimary} from '../../../shared/components/button-primary/button-primary';
+import {CheckboxForm} from '../../../shared/components/checkbox-form/checkbox-form';
 
 @Component({
   selector: 'app-register',
   imports: [
     InputForm,
     ReactiveFormsModule,
-    ButtonPrimary
+    ButtonPrimary,
+    CheckboxForm
   ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
@@ -18,7 +20,8 @@ export class Register {
     nom: new FormControl('', [Validators.required]),
     prenom: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    dateNaissance: new FormControl('', [Validators.required])
+    dateNaissance: new FormControl('', [Validators.required]),
+    consentement: new FormControl('')
   });
 
   onSubmit(): void {
