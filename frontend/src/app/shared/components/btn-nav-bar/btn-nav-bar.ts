8c23baa62afc5c940229f5dfccb9a80eb8bef5d3
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-07 10:08:55 +0100

    feat (btn-nav-bar) : Add icon support to nav bar buttons

--- frontend/src/app/shared/components/btn-nav-bar/btn-nav-bar.ts
+++ frontend/src/app/shared/components/btn-nav-bar/btn-nav-bar.ts
@@ -1,16 +1,17 @@
-import { Component, Input} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-btn-nav-bar',
   imports: [
-    RouterLinkActive,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './btn-nav-bar.html',
   styleUrl: './btn-nav-bar.scss',
 })
 export class BtnNavBar {
-  @Input() label: string | undefined;
-  @Input() link: string | undefined;
+  @Input() label: string = '';
+  @Input() link: string = '';
+  @Input() icon: string = '';
 }
--- frontend/src/app/shared/components/nav-bar/nav-bar.ts
+++ frontend/src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component} from '@angular/core';
+import {BtnNavBar} from '../btn-nav-bar/btn-nav-bar';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
-    RouterLink
+    BtnNavBar
+
   ],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
 export class NavBar {
-
+  customLink = [
+    {label: 'Accueil', link: '/', icon: 'home'},
+    {label: 'Quiz', link: '/quiz', icon: 'user-plus'},
+    {label: 'Ateliers', link: '/atelier', icon: 'sign-in-alt'}
+  ];
 }
