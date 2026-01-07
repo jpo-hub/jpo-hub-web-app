106163e8bf8ede155b865e8c2d1c92edff7c58b6
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-07 12:23:43 +0100

    feat (app.routes.ts) : Add Home component route to app routes
    Configured the root path ('') to render the Home component. This ensures users are directed to the Home page when accessing the base URL.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {NotFound} from './shared/pages/not-found/not-found';
+import {Home} from './pages/client/home/home';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: '**', component: NotFound }
 ];
