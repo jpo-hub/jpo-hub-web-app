eafdabb92247b605a296c4e39fcda099a1b1895a
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-07 14:27:28 +0100

    feat (app.routes.ts) : Add register route to app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {NotFound} from './shared/pages/not-found/not-found';
 import {Home} from './pages/client/home/home';
+import {Register} from './pages/client/register/register';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'quiz/register', component: Register },
   { path: '**', component: NotFound }
 ];
