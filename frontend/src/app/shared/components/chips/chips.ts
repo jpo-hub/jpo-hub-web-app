a7f45e0f000dfe4ecca9061a6aa9c16cdcf28d63
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-08 10:03:54 +0100

    feat (components) : Add chips component

---
+++ frontend/src/app/shared/components/chips/chips.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chips',
+  imports: [],
+  templateUrl: './chips.html',
+  styleUrl: './chips.scss',
+})
+export class Chips {
+
+}
