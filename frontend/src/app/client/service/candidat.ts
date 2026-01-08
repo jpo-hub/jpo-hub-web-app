fd9f9dc7d0bc52c6161edac2fdb75abc13763b64
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-08 18:35:05 +0100

    ci (service/candidat) : Add submitCandidature method to Candidat service

--- frontend/src/app/client/service/candidat.ts
+++ frontend/src/app/client/service/candidat.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '@environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {CandidatModel} from '../../core/models/candidat.model';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class Candidat {
-  
+  private apiUrl = environment.apiURL;
+
+  constructor(private http: HttpClient) {}
+
+  public submitCandidature(formData: CandidatModel) {
+    return this.http.post(`${this.apiUrl}/candidats`, formData);
+  }
 }
