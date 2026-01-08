e2b82398577bc585d3857683f8cb33bc6a5b6ff4
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-08 10:51:03 +0100

    ci (all) : Enable CORS in backend and update HTTP client in frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200', // ton frontend
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true, // si besoin d’envoyer des cookies
+  });
+
   const config = new DocumentBuilder()
     .setTitle('API JPO-HUB')
     .setDescription('The JPO-HUB API description')
@@ -26,6 +32,7 @@ async function bootstrap() {
       supportedSubmitMethods: ['get', 'post', 'put', 'patch', 'delete'],
     },
   });
+
   await app.listen(3000);
 }
 bootstrap();
