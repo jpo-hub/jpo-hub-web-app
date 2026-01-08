012b2dcaf765c0f1d069ab8d7eb246ed0fc023a4
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-08 09:19:44 +0100

    ci (client) : Move client home and register pages to new directory

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {NotFound} from './shared/pages/not-found/not-found';
-import {Home} from './pages/client/home/home';
-import {Register} from './pages/client/register/register';
+import {Home} from './client/pages/home/home';
+import {Register} from './client/pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: Home },
