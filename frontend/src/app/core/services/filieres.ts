491e594346ff38845ecf0ad6eb8dcfee73023626
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-08 10:04:29 +0100

    feat (service) : add filieres service

--- frontend/src/app/core/services/filieres.ts
+++ frontend/src/app/core/services/filieres.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '@environments/environment';
+import {BehaviorSubject, catchError, Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Filiere} from '../models/filiere.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Filieres {
-  
+  private apiUrl = environment.apiURL;
+
+  private filieresSubject = new BehaviorSubject<Filiere[]>([]);
+  public filieres: Observable<Filiere[]> = this.filieresSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  public getFilieres(): void {
+    this.http
+      .get<Filiere[]>(`${this.apiUrl}/filieres`)
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching filieres:', error);
+          return of([]);
+        })
+      )
+      .subscribe((filieres) => {
+        this.filieresSubject.next(filieres);
+      });
+  }
 }
