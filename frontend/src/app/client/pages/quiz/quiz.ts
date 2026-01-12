b2532f64f4aacbfb89dcbe122848397d30e4c8d1
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-12 10:34:00 +0100

    feat (quiz) : Implement quiz page UI and logic

--- frontend/src/app/client/pages/quiz/quiz.ts
+++ frontend/src/app/client/pages/quiz/quiz.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { ProcessBar } from '../../components/process-bar/process-bar';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
-  imports: [],
+  imports: [
+    ProcessBar,
+    DecimalPipe
+  ],
   templateUrl: './quiz.html',
   styleUrl: './quiz.scss',
 })
 export class Quiz {
+  currentQuestion = signal(0);
+  totalQuestions = signal(10);
 
+  progressPercent = computed(() => {
+    return Math.min(100, Math.max(0, (this.currentQuestion() / this.totalQuestions()) * 100));
+  });
+
+  nextQuestion() {
+    console.log("cece");
+    if (this.currentQuestion() < this.totalQuestions()) {
+      this.currentQuestion.update(value => value + 1);
+    }
+    console.log(this.currentQuestion());
+  }
 }
