610e194099e1c9c3f38719fd821563176d45b228
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-12 13:51:41 +0100

    ci (quiz) : Refactor quiz question handling and fix model typo

--- frontend/src/app/client/pages/quiz/quiz.ts
+++ frontend/src/app/client/pages/quiz/quiz.ts
@@ -1,10 +1,11 @@
-import { Component, computed, OnInit, signal, inject } from '@angular/core';
-import { ProcessBar } from '../../components/process-bar/process-bar';
-import { DecimalPipe } from '@angular/common';
-import { Questions } from '../../../core/services/questions';
-import { Question } from '../../../core/models/question.model';
-import { toSignal } from '@angular/core/rxjs-interop';
+import {Component, computed, effect, inject, signal} from '@angular/core';
+import {ProcessBar} from '../../components/process-bar/process-bar';
+import {DecimalPipe} from '@angular/common';
+import {Questions} from '../../../core/services/questions';
+import {Question} from '../../../core/models/question.model';
+import {toObservable, toSignal} from '@angular/core/rxjs-interop';
 import {ButtonPrimary} from '../../../shared/components/button-primary/button-primary';
+import {filter, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz',
@@ -16,11 +17,10 @@ import {ButtonPrimary} from '../../../shared/components/button-primary/button-pr
   templateUrl: './quiz.html',
   styleUrl: './quiz.scss',
 })
-export class Quiz implements OnInit {
+export class Quiz {
   private questionsServices = inject(Questions);
 
   currentQuestion = signal(0);
-
   questions = toSignal(this.questionsServices.questions$, { initialValue: [] as Question[] });
 
   totalQuestions = computed(() => this.questions().length);
@@ -35,15 +35,31 @@ export class Quiz implements OnInit {
     return this.questions()[this.currentQuestion()];
   });
 
-  ngOnInit() {
+  // ✅ Solution : Convertir le computed en Observable puis le reconvertir en Signal
+  currentQuestionDetails = toSignal(
+    toObservable(this.currentQuestionData).pipe(
+      filter(question => question?.uid != null), // Filtre les valeurs undefined
+      switchMap(question => this.questionsServices.getQuestionByUid(question.uid))
+    ),
+    { initialValue: null } // Valeur par défaut pendant le chargement
+  );
+
+  constructor() {
     this.questionsServices.loadQuestions();
+
+    effect(() => {
+      this.currentQuestionData();
+      this.currentQuestionDetails();
+
+      console.log(this.currentQuestionDetails());
+    });
   }
 
   nextQuestion() {
     if (this.currentQuestion() < this.totalQuestions() - 1) {
       this.currentQuestion.update(value => value + 1);
     } else {
-      console.log('Quiz terminé !');
+      this.finishQuiz();
     }
   }
 
--- frontend/src/app/core/models/question.model.ts
+++ frontend/src/app/core/models/question.model.ts
@@ -4,5 +4,5 @@ export class Question {
   uid!: string;
   label!: string;
   multiple!: boolean;
-  Responses!: Responses[];
+  reponses!: Responses[];
 }
