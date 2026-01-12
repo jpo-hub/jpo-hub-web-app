aadb0b28223010c01bb4b0248525de852f18eb88
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-12 09:59:14 +0100

    feat (app.routes.ts) : Add route for Quiz component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {NotFound} from './shared/pages/not-found/not-found';
 import {Home} from './client/pages/home/home';
 import {Register} from './client/pages/register/register';
+import {Quiz} from './client/pages/quiz/quiz';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'quiz', component: Quiz },
   { path: 'quiz/register', component: Register },
   { path: '**', component: NotFound }
 ];
