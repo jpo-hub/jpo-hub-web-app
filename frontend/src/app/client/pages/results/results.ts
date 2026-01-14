45746e450776ebc3b3f7fefe6b063ed4f74a2044
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-14 14:41:44 +0100

    feat (results) : add logique for display ateliers

--- frontend/src/app/client/pages/results/results.ts
+++ frontend/src/app/client/pages/results/results.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, effect } from '@angular/core';
+import { Candidat } from '../../service/candidat';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-results',
+  standalone: true,
   imports: [],
   templateUrl: './results.html',
   styleUrl: './results.scss',
 })
 export class Results {
+  private candidatService = inject(Candidat);
 
+  scoringData = toSignal(this.candidatService.getScoring(), { initialValue: null });
+
+  constructor() {
+    effect(() => {
+      const data = this.scoringData();
+      if (data) {
+        console.log('Signal mis à jour avec les données du back :', data);
+      }
+    });
+  }
 }
