909535b0472caaffd287d38c691e7622ff11592b
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-14 11:55:02 +0100

    feat (app.routes.ts) : add routes for results component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {Home} from './client/pages/home/home';
 import {Register} from './client/pages/register/register';
 import {Quiz} from './client/pages/quiz/quiz';
 import {quizGuard} from './core/guards/quiz-guard';
+import {Results} from './client/pages/results/results';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'quiz', component: Quiz, canActivate: [quizGuard] },
   { path: 'quiz/register', component: Register },
+  { path: 'quiz/results', component: Results },
   { path: '**', component: NotFound }
 ];
