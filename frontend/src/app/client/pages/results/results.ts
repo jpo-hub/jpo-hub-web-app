1ba523ae325ec5e6920f5d7b570a14dd85762ffb
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-15 13:48:21 +0100

    feat (results) : add import component card-atelier

--- frontend/src/app/client/pages/results/results.ts
+++ frontend/src/app/client/pages/results/results.ts
@@ -1,11 +1,14 @@
 import { Component, inject, effect } from '@angular/core';
 import { Candidat } from '../../service/candidat';
 import { toSignal } from '@angular/core/rxjs-interop';
+import {CardAtelier} from '../../components/card-atelier/card-atelier';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [
+    CardAtelier
+  ],
   templateUrl: './results.html',
   styleUrl: './results.scss',
 })
