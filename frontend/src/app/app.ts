9f50b78c099802e4d7acd277c3bbda1c5f3097f9
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-19 14:31:42 +0100

    ci (app.ts) : remove nav bar for routes admin/login

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
-import {Component, inject} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, computed, inject} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {NavBar} from './shared/components/nav-bar/nav-bar';
 import {Toast} from './shared/components/toast/toast';
 import {ModalService} from './client/services/modal';
 import {ModalParticipation} from './client/components/modal-participation/modal-participation';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,15 @@ import {ModalParticipation} from './client/components/modal-participation/modal-
 })
 export class App {
   modalService = inject(ModalService);
+  private router = inject(Router);
+
+  private currentUrl = toSignal(
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => event.urlAfterRedirects)
+    )
+  );
+
+  showNavBar = computed(() => this.currentUrl() !== '/admin/login');
+
 }
