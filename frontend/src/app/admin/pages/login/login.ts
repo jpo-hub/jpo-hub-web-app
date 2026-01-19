390debffe4c30719055b089eeeac24ab4459109c
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-19 11:48:59 +0100

    feat (login.html) : add form login

--- frontend/src/app/admin/pages/login/login.ts
+++ frontend/src/app/admin/pages/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {InputForm} from '../../../shared/components/input-form/input-form';
+import {ButtonPrimary} from '../../../shared/components/button-primary/button-primary';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    InputForm,
+    ButtonPrimary,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
