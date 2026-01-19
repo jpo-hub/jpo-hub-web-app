9ecfb1cd33e69500b38d42f7ba3b7dbbb6031202
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-19 09:41:50 +0100

    feat (app) : add modal

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavBar} from './shared/components/nav-bar/nav-bar';
 import {Toast} from './shared/components/toast/toast';
+import {ModalService} from './client/services/modal';
 import {ModalParticipation} from './client/components/modal-participation/modal-participation';
 
 @Component({
@@ -11,4 +12,5 @@ import {ModalParticipation} from './client/components/modal-participation/modal-
   styleUrl: './app.scss'
 })
 export class App {
+  modalService = inject(ModalService);
 }
