f24a11de18966898bb3793935d92185d03ce0fdf
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-19 09:19:57 +0100

    feat (app.html) : add modal in app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavBar} from './shared/components/nav-bar/nav-bar';
 import {Toast} from './shared/components/toast/toast';
+import {ModalParticipation} from './client/components/modal-participation/modal-participation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Toast],
+  imports: [RouterOutlet, NavBar, Toast, ModalParticipation],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
