3fe2f9fb115cf5267821bc6d532294e6bf8099af
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-19 08:56:08 +0100

    feat (results) : add firstname in message

--- frontend/src/app/client/pages/results/results.ts
+++ frontend/src/app/client/pages/results/results.ts
@@ -1,20 +1,25 @@
-import { Component, inject, effect } from '@angular/core';
-import { Candidat } from '../../service/candidat';
-import { toSignal } from '@angular/core/rxjs-interop';
+import {Component, effect, inject} from '@angular/core';
+import {Candidat} from '../../service/candidat';
+import {toSignal} from '@angular/core/rxjs-interop';
 import {CardAtelier} from '../../components/card-atelier/card-atelier';
+import {StorageService} from '../../../core/services/storage-service';
+import {TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-results',
   standalone: true,
   imports: [
-    CardAtelier
+    CardAtelier,
+    TitleCasePipe
   ],
   templateUrl: './results.html',
   styleUrl: './results.scss',
 })
 export class Results {
   private candidatService = inject(Candidat);
+  private storageService = inject(StorageService);
 
+  candidatName = this.storageService.getCandidatName() || null;
   scoringData = toSignal(this.candidatService.getScoring(), { initialValue: null });
 
   constructor() {
