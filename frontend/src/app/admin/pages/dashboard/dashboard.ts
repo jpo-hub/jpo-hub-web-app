2c52cd5ae042dd9bf267c9d3ccbf85c6cb6d859f
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-20 14:22:32 +0100

    feat (dashboard.ts) : import StatCard

--- frontend/src/app/admin/pages/dashboard/dashboard.ts
+++ frontend/src/app/admin/pages/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {StatCard} from '../../components/stat-card/stat-card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    StatCard
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
