c16a4c0387a80b8d863ca14b485add7d208d9d0e
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-20 14:36:18 +0100

    feat (candidat.ts) :  add catch error

--- frontend/src/app/admin/services/candidat.ts
+++ frontend/src/app/admin/services/candidat.ts
@@ -1,18 +1,32 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable } from '@angular/core';
 import {environment} from '@environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {CandidatModel} from '../../core/models/candidat.model';
+import {catchError, throwError} from 'rxjs';
+import {ToastService} from '../../core/services/toast';
+import {ErrorHandler} from '../../core/services/error-handler';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Candidat {
   apiUrl = environment.apiURL;
+  private toastService = inject(ToastService);
+  private errorHandler = inject(ErrorHandler);
 
   constructor(
     private http: HttpClient
   ) { }
 
   public getCandidats() {
-    return this.http.get(`${this.apiUrl}/candidats`);
+    return this.http.get<CandidatModel[]>(`${this.apiUrl}/candidats`).pipe(
+      catchError(err => {
+        const error = err as HttpErrorResponse;
+
+        const message = this.errorHandler.getErrorMessage(error.error.code);
+        this.toastService.show(message, 'danger');
+        return throwError(() => err);
+      })
+    );
   }
 }
