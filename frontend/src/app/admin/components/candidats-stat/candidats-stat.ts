e5983773461018574b5b6d2bafa9107679396212
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-21 11:55:20 +0100

    feat (candidat-stat.ts) : add import CandidatsInfoDahsboard

--- frontend/src/app/admin/components/candidats-stat/candidats-stat.ts
+++ frontend/src/app/admin/components/candidats-stat/candidats-stat.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {Candidat} from '../../services/candidat';
+import {CandidatModel} from '../../../core/models/candidat.model';
+import {CandidatsInfoDashboard} from '../candidats-info-dashboard/candidats-info-dashboard';
 
 @Component({
   selector: 'app-candidats-stat',
-  imports: [],
+  imports: [
+    CandidatsInfoDashboard
+  ],
   templateUrl: './candidats-stat.html',
   styleUrl: './candidats-stat.scss',
 })
 export class CandidatsStat {
+  private candidatService = inject(Candidat);
 
+  candidats = toSignal(this.candidatService.getCandidats(), { initialValue: [] as CandidatModel[] });
 }
