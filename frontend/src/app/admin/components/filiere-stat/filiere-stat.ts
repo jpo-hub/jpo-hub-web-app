9c91775c9566a7262571106aaad37a5d996d676f
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-21 15:06:23 +0100

    feat (filiere-stat.ts) : call service api

--- frontend/src/app/admin/components/filiere-stat/filiere-stat.ts
+++ frontend/src/app/admin/components/filiere-stat/filiere-stat.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
+import {Stats} from '../../services/stats';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-filiere-stat',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './filiere-stat.scss',
 })
 export class FiliereStat {
+  private statsService = inject(Stats);
 
+  stats = toSignal(this.statsService.getStats(), { initialValue: [] as any[] });
+
+  constructor() {
+    effect(() => {
+      console.log(this.stats());
+    });
+  }
 }
