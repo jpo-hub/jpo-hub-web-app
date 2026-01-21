879612a8b950da7d368d74869da006cec95369f0
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-01-21 11:14:59 +0100

    feat (components) : add candidats-stat components

---
+++ frontend/src/app/admin/components/candidats-stat/candidats-stat.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidats-stat',
+  imports: [],
+  templateUrl: './candidats-stat.html',
+  styleUrl: './candidats-stat.scss',
+})
+export class CandidatsStat {
+
+}
