d3b3a8e8b35d1f98d773f307958f6f51a42ef655
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-05-28 14:04:49 +0200

    Add quiz footer and layout adjustments

--- frontend/src/app/client/pages/ateliers/ateliers.ts
+++ frontend/src/app/client/pages/ateliers/ateliers.ts
@@ -2,11 +2,13 @@ import {Component, effect, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {Ateliers as AtelierService} from '../../services/ateliers';
 import {CardAtelier} from '../../components/card-atelier/card-atelier';
+import {ButtonPrimary} from '../../../shared/components/button-primary/button-primary';
 
 @Component({
   selector: 'app-ateliers',
   imports: [
-    CardAtelier
+    CardAtelier,
+    ButtonPrimary
   ],
   templateUrl: './ateliers.html',
   styleUrl: './ateliers.scss',
